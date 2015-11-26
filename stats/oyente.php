57bb9affa62305aa3f1664d543fa9a98d4db3917
Edin Melchor <edin.sof@gmail.com>
2015-11-26 02:34:55 -0500

    Puertos
    Solucionado error de puertos

--- stats/oyente.php
+++ stats/oyente.php
@@ -3,7 +3,7 @@ $port = $_REQUEST['p'];
 $puerto = base64_decode($port);
 
 $curl_handle=curl_init();
-curl_setopt($curl_handle, CURLOPT_URL,$_SERVER['SERVER_ADDR'].':'.$puerto.'/panel.xsl');
+curl_setopt($curl_handle, CURLOPT_URL,$_SERVER['SERVER_NAME'].':'.$puerto.'/panel.xsl');
 curl_setopt($curl_handle, CURLOPT_CONNECTTIMEOUT, 2);
 curl_setopt($curl_handle, CURLOPT_RETURNTRANSFER, 1);
 curl_setopt($curl_handle,CURLOPT_USERAGENT,'Mozilla/5.0 (Windows; U; Windows NT 5.1; es-CO; rv:1.8.1.13) Gecko/20080311 Firefox/2.0.0.13');
